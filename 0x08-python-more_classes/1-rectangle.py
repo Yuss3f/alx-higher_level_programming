ec1a8ff53e5c3e5661bac84a1e8d524151fdd4a9
Kayk Youssef <kayke8youssef@gmail.com>
2024-01-29 06:35:56 -0800

    Python3

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,40 +1,38 @@
 #!/usr/bin/python3
-"""
-Defines a class Rectangle
-"""
+"""Define Rectangle Class"""
 
 
 class Rectangle:
-    """Representation of a rectangle"""
-    def _init_(self, width=0, height=0):
-        """Initializes the rectangle"""
+    """Representation of a Rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initializes the Rectangle"""
         self.height = height
         self.width = width
 
     @property
     def width(self):
-        """getter for the private instance attribute width"""
-        return self._width
+        """get for the private instance attr width"""
+        return self.__width
 
     @width.setter
     def width(self, value):
-        """setter for the private instance attribute width"""
+        """set for the private instance attr width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
-        """getter for the private instance attribute height"""
-        return self._height
+        """get for the private instance attr height"""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """setter for the private instance attribute height"""
+        """set for the private instance attr height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be >=0")
-        self._height = value
+            raise ValueError("height must be >= 0")
+        self.__height = value
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""
-Defines a class Rectangle
-"""
+"""Defines a class Rectangle"""
 
 
 class Rectangle:
-    """Represents a rectangle"""
+    """Representation of a rectangle"""
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
         self.width = width
@@ -29,7 +27,7 @@ class Rectangle:
     def height(self):
         """getter for the private instance attribute height"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """setter for the private instance attribute height"""
@@ -47,4 +45,4 @@ class Rectangle:
         """returns the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.__width * 2) + (self._height * 2)
+        return (self.__width * 2) + (self.__height * 2)
